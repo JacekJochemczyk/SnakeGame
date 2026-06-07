60bf7b68c97153f5eae0ce2f219e9322901fb84c
LukaszChodzicki <chodzicki.lukasz@gmail.com>
2026-06-07 11:33:23 +0200

    Naprawa komunikatu Game Over

--- SnakeGame/Snake.cs
+++ SnakeGame/Snake.cs
@@ -206,7 +206,7 @@ class Program
 
             //Hindernis treffen
 
-            if (hoofd.xPos == obstacleXpos /* ?? */ == obstacleYpos)
+            if (hoofd.xPos == obstacleXpos && hoofd.yPos == obstacleYpos)
 
             {
                 score++;
